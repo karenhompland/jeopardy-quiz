aad99c837917a61d83f6ec039a1b63f37424607e
karenhompland <khompland@gmail.com>
2023-05-05 00:16:06 +0200

    started functionality to edit questions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var scoreList;
 var editMode = false;
 var numOfPlayers;
+var buttonText = [];
 
 function onLoad() {
     readTextFile("./spm.json", function (text) {
@@ -9,8 +10,8 @@ function onLoad() {
         img = document.getElementById('qImg');
     });
     checkLocalStorage();
-    for (const x of Array(numOfPlayers).keys()){
-        document.getElementById(`team${x+1}`).style.display="flex";
+    for (const x of Array(numOfPlayers).keys()) {
+        document.getElementById(`team${x + 1}`).style.display = "flex";
     }
 }
 
@@ -30,15 +31,15 @@ function checkLocalStorage() {
     if (localStorage.getItem("heading") != null) {
         document.getElementById("heading").innerHTML = localStorage.getItem("heading");
     }
-    localStorage.getItem('numPlayers')!= null ? numOfPlayers = parseInt(localStorage.getItem('numPlayers')) : numOfPlayers=3 ;
+    localStorage.getItem('numPlayers') != null ? numOfPlayers = parseInt(localStorage.getItem('numPlayers')) : numOfPlayers = 3;
     if (localStorage.getItem('scoreList') != null) {
         scoreList = localStorage.getItem('scoreList').split(",").map(function (item) {
             return parseInt(item);
         })
         updateAllScores();
     }
-    else { 
-        scoreList=Array(numOfPlayers).fill(0);
+    else {
+        scoreList = Array(numOfPlayers).fill(0);
     }
     for (const x of Array(5).keys()) {
         var category = localStorage.getItem(`category${x + 1}`);
@@ -51,9 +52,9 @@ function checkLocalStorage() {
     }
 }
 
-function updateAllScores(){
-    for (const x of Array(scoreList.length).keys()){
-        document.getElementById(`score${x+1}`).innerHTML=scoreList[x];
+function updateAllScores() {
+    for (const x of Array(scoreList.length).keys()) {
+        document.getElementById(`score${x + 1}`).innerHTML = scoreList[x];
     }
 
 }
@@ -66,18 +67,37 @@ function enableEditMode() {
     document.getElementById("playButton").style.display = "block";
     document.getElementById("editButton").style.display = "none";
     document.getElementById("restartButton").style.display = "none";
+    const qButtons = document.querySelectorAll('div.container > button');
+    qButtons.forEach(element => {
+        buttonText.push(element.innerHTML);
+        localStorage.getItem(element.id) == null ? element.innerHTML = `<p contentEditable=true></p>` : element.innerHTML = `<p contentEditable=true>${localStorage.getItem(element.id)}</p>`;
+        element.style.backgroundColor = "#2a2a2a";
+        element.style.color = "#ffffff";
+    });
     const editableElements = document.querySelectorAll('.editable');
     editableElements.forEach(element => {
         element.contentEditable = "true";
-        element.style.textDecoration="underline 2px #FFFFFF99";
+        element.style.textDecoration = "underline 2px #FFFFFF99";
     });
+    /*const qContainer = document.getElementById("qContainer");
+    qContainer.addEventListener("keydown", function (event) {
+        if (event.target.nodeName === "BUTTON" && event.key === " ") {
+            event.preventDefault();
+            event.target.innerHTML+=" ";
+        }
+    });*/
 }
+
 
 function saveButton() {
     localStorage.setItem("heading", document.getElementById("heading").innerHTML);
     for (const x of Array(5).keys()) {
         localStorage.setItem(`category${x + 1}`, document.getElementById(`category${x + 1}`).innerHTML);
     }
+    const qButtons = document.querySelectorAll('div.container > button');
+    qButtons.forEach(element => {
+        localStorage.setItem(element.id, element.firstChild.innerHTML);
+    });
 }
 
 function playMode() {
@@ -89,17 +109,24 @@ function playMode() {
     document.getElementById("playButton").style.display = "none";
     document.getElementById("editButton").style.display = "block";
     document.getElementById("restartButton").style.display = "block";
+    const qButtons = document.querySelectorAll('div.container > button');
+    var counter = 0;
+    qButtons.forEach(element => {
+        element.innerHTML = buttonText[counter];
+        counter++;
+        element.style = "";
+    });
     const editableElements = document.querySelectorAll('.editable');
     editableElements.forEach(element => {
         element.contentEditable = "false";
-        element.style.textDecoration="none";
+        element.style.textDecoration = "none";
     });
 }
 
 function resetGame() {
     if (confirm("Do you want to reset the game? This will clear all scores.")) {
-        scoreList=Array(numOfPlayers).fill(0);
-        localStorage.setItem('scoreList',scoreList);
+        scoreList = Array(numOfPlayers).fill(0);
+        localStorage.setItem('scoreList', scoreList);
         updateAllScores();
         document.querySelectorAll('.opened').forEach(element => {
             element.style = "";
@@ -132,19 +159,23 @@ function onClick(id) {
             audio.play();
         }
     }
+    else if (editMode) {
+
+
+    }
 
 
 }
 
 function scoreClickMin(id) {
-    scoreList[id-1] -= 1;
-    document.getElementById(`score${id}`).innerHTML = scoreList[id-1];
+    scoreList[id - 1] -= 1;
+    document.getElementById(`score${id}`).innerHTML = scoreList[id - 1];
     localStorage.setItem('scoreList', scoreList);
 }
 
 function scoreClickPlus(id) {
-    scoreList[id-1] += 1;
-    document.getElementById(`score${id}`).innerHTML = scoreList[id-1];
+    scoreList[id - 1] += 1;
+    document.getElementById(`score${id}`).innerHTML = scoreList[id - 1];
     localStorage.setItem('scoreList', scoreList);
 }
 
@@ -155,39 +186,39 @@ function closeQ() {
     img.style.visibility = "hidden";*/
 }
 
-function plusPlayer(){
-    if(numOfPlayers<5){
-        numOfPlayers+=1;
-        if(numOfPlayers==1){
-            scoreList=[0];
+function plusPlayer() {
+    if (numOfPlayers < 5) {
+        numOfPlayers += 1;
+        if (numOfPlayers == 1) {
+            scoreList = [0];
         }
         else {
             scoreList.push(0);
         }
-        document.getElementById(`team${numOfPlayers}`).style.display="flex";
+        document.getElementById(`team${numOfPlayers}`).style.display = "flex";
         //document.getElementById(`team${numOfPlayers}`).innerHTML=scoreList[numOfPlayers];
     }
-    if(numOfPlayers==5){
-        document.getElementById("plusPlayer").disabled = true; 
+    if (numOfPlayers == 5) {
+        document.getElementById("plusPlayer").disabled = true;
     }
     document.getElementById("minPlayer").disabled = false;
     savePlayers();
 }
 
-function minPlayer(){
-    if(numOfPlayers>0){
-        document.getElementById(`team${numOfPlayers}`).style.display="none";
-        numOfPlayers-=1;
+function minPlayer() {
+    if (numOfPlayers > 0) {
+        document.getElementById(`team${numOfPlayers}`).style.display = "none";
+        numOfPlayers -= 1;
         scoreList.pop();
     }
-    if(numOfPlayers==0){
-        document.getElementById("minPlayer").disabled = true; 
+    if (numOfPlayers == 0) {
+        document.getElementById("minPlayer").disabled = true;
     }
     document.getElementById("plusPlayer").disabled = false;
     savePlayers();
 }
 
-function savePlayers(){
+function savePlayers() {
     localStorage.setItem("numPlayers", numOfPlayers);
     localStorage.setItem("scoreList", scoreList);
 }
